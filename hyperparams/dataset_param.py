af5f15e8bd023442899a759ee5ce06cb3e11b56c
Through Iris <47046649+ThroughIris@users.noreply.github.com>
2023-03-03 10:26:07 -0800

    Update dataset_param.py

--- hyperparams/dataset_param.py
+++ hyperparams/dataset_param.py
@@ -2,16 +2,16 @@ class DatasetParam:
     """
     Hyper-parameters for utils/dataset.py
     """
-    img_w = 450  # image width, should be EVEN, or the final .mp4 video may be crashed
-    img_h = 350  # image height, should be EVEN, or the final .mp4 video may be crashed
+    img_w = 720  # image width, should be EVEN, or the final .mp4 video may be crashed
+    img_h = 1280  # image height, should be EVEN, or the final .mp4 video may be crashed
 
     use_video = True  # Style transfer with a video or an image. True: video; False: image
     content_img_path = r'demo/dog.jpg'
-    video_path = r'demo/short_video.mp4'
-    style_img_path = r'demo/mrbean.png'
+    video_path = r'demo/CaseStudy01_Chasing_the_feeling_v011.mp4'
+    style_img_path = r'demo/PaintingStrokes_03.png'
 
     img_fmt = 'jpg'  # frame image format
-    video_fps = 24  # select `video_fps` frames per second
+    video_fps = 30  # select `video_fps` frames per second
 
     # method to calculate optic flow
     # 'dm_df2': DeepMatching + DeepFlow2, the best quality
