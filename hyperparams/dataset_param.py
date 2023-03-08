994044321ce6ac431b4812b5d12063b2df47fade
Through Iris <47046649+ThroughIris@users.noreply.github.com>
2023-03-08 14:34:13 -0800

    Update dataset_param.py

--- hyperparams/dataset_param.py
+++ hyperparams/dataset_param.py
@@ -8,7 +8,7 @@ class DatasetParam:
     use_video = True  # Style transfer with a video or an image. True: video; False: image
     content_img_path = r'demo/CaseStudy02_alone_v018_Still.jpg'
     video_path = r'demo/CaseStudy02_alone_v018_ShortTest.mp4'
-    style_img_path = r'demo/GraphicDesign_14.jpg'
+    style_img_path = r'demo/PaintingStrokes_13.jpg'
 
     img_fmt = 'jpg'  # frame image format
     video_fps = 30  # select `video_fps` frames per second
@@ -26,4 +26,4 @@ class DatasetParam:
     # 'image': use the current video frame image
     # 'image_flow_warp': warp the previous stylized image with optic flow
     # 'random': normal distribution
-    init_generated_image_method = 'image'
+    init_generated_image_method = 'image_flow_warp'
